bfaf828655f462c669a5676d9b2de9152bf87430
Eric Bragas <ericbragas412@gmail.com>
2019-02-06 13:17:39 -0800

    Discovered undocumented API. Yes! Remove spider

--- go-spider.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from panna_recipe_scraper.spiders.recipe_spider import RecipeSpider
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(RecipeSpider)
-process.start()
